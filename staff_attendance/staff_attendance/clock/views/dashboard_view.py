1a3f53b5dca90a02de18b96c1f5146423c82af4a
zayinyod <mishimashuji1564@yahoo.co.jp>
2026-08-29 00:43:02 +0900

    Fixed: ダッシュボードの日付パラメータを検証

--- staff_attendance/staff_attendance/clock/views/dashboard_view.py
+++ staff_attendance/staff_attendance/clock/views/dashboard_view.py
@@ -1,6 +1,7 @@
 from util.mixins import CustomLoginRequiredMixin
-from datetime import datetime
 from django.shortcuts import render
+from django.utils import timezone
+from django.utils.dateparse import parse_date
 from django.views import View
 from ..usecases import ClockCalculate
 
@@ -8,15 +9,30 @@ class DashboardView(CustomLoginRequiredMixin, View):
     template_name = "clock/dashboard.html"
 
     def get(self, request):
-        user = request.user
-        date = request.GET.get("date")
-
-        if not date:
-            date = datetime.today().date()
-
-        summary = ClockCalculate.daily_summary(user, date)
+        date, has_invalid_date = self.resolve_date(request.GET.get("date"))
+        summary = ClockCalculate.daily_summary(request.user, date)
         context = {
             "summary": summary,
             "date": date,
+            "has_invalid_date": has_invalid_date,
         }
         return render(request, self.template_name, context)
+
+    @staticmethod
+    def resolve_date(value: str) -> tuple:
+        """
+        クエリパラメータの日付をdate型に変換する。
+        未指定もしくは不正な値の場合は当日を返す。
+        """
+        if not value:
+            return timezone.localdate(), False
+
+        try:
+            parsed_date = parse_date(value)
+        except ValueError:
+            parsed_date = None
+
+        if parsed_date is None:
+            return timezone.localdate(), True
+
+        return parsed_date, False
