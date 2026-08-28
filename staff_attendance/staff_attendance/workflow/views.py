339924fb9dd91af9d517bc3dc20c5ef19f41976b
zayinyod <mishimashuji1564@yahoo.co.jp>
2026-08-29 00:40:38 +0900

    Fixed: 権限チェックを dispatch に集約し、承認処理の入力検証を追加

--- staff_attendance/staff_attendance/workflow/views.py
+++ staff_attendance/staff_attendance/workflow/views.py
@@ -1,6 +1,7 @@
 from util.base_superuser_view import BaseSuperUserView
 from util.base_user_view import BaseUserView
-from django.shortcuts import render, redirect
+from django.db import transaction
+from django.shortcuts import redirect
 from .paid_leave.usecases import PaidLeaveEntry
 
 class WorkflowSelectView(BaseSuperUserView):
@@ -12,17 +13,32 @@ class RequestView(BaseUserView):
 class ApprovalView(BaseSuperUserView):
     template = "workflow/approval.html"
 
+    status_prefix = "status_"
+    approvable_codes = ("1", "2")
+
     def get_context_data(self, request):
         return {"pending_list": PaidLeaveEntry.pending()}
 
     def post(self, request):
-        if not request.user.is_superuser:
-            return render(request, self.forbidden_template, status=403)
+        with transaction.atomic():
+            for key, value in request.POST.items():
+                request_id = self.extract_request_id(key)
+                if request_id is None or value not in self.approvable_codes:
+                    continue
 
-        for key, value in request.POST.items():
-            if key.startswith("status_"):
-                _, id = key.split("_")
-                if value in ["1", "2"]:
-                    PaidLeaveEntry.approve(id, value, request.user)
+                PaidLeaveEntry.approve(request_id, value, request.user)
 
         return redirect("approval")
+
+    @classmethod
+    def extract_request_id(cls, key: str) -> str:
+        """
+        POSTキーから申請IDを取り出す。
+        対象外のキーおよび数値でないIDはNoneを返す。
+        """
+        if not key.startswith(cls.status_prefix):
+            return None
+
+        request_id = key.removeprefix(cls.status_prefix)
+
+        return request_id if request_id.isdecimal() else None
--- staff_attendance/util/base_superuser_view.py
+++ staff_attendance/util/base_superuser_view.py
@@ -11,12 +11,18 @@ class BaseSuperUserView(CustomLoginRequiredMixin, View):
     template = None
     forbidden_template = "403.html"
 
+    def dispatch(self, request, *args, **kwargs):
+        """
+        HTTPメソッドを問わず権限を検証する。
+        未認証の場合はCustomLoginRequiredMixinがログインページへ誘導する。
+        """
+        if request.user.is_authenticated and not request.user.is_superuser:
+            return render(request, self.forbidden_template, status=403)
+
+        return super().dispatch(request, *args, **kwargs)
+
     def get_context_data(self, request) -> dict:
         return {}
 
     def get(self, request):
-        if not request.user.is_superuser:
-            return render(request, self.forbidden_template, status=403)
-        context = self.get_context_data(request)
-
-        return render(request, self.template, context)
+        return render(request, self.template, self.get_context_data(request))
